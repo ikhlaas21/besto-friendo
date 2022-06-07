b8ca32ecb023d85a3e1cc77fa8e3cc8a5fe14e8a
ikhlaas21 <ikhlaas21.rawoot@gmail.com>
2022-06-07 10:58:45 +0200

    friendsjava

---
+++ code.js
@@ -0,0 +1,51 @@
+let age = 18;
+let salary = 5000;
+let bonus = 500;
+
+///statement 1
+// if((age >=18)  && (salary >=5000)){
+//     salary += bonus;
+//     //salary = salary = bonus;
+//     console.log('your salary:R'.concat(salary))
+
+// } else {
+//     console.log('please try again later')
+// }
+
+///statement 2
+
+// if(age >= 18) {
+//     if (salary >= 5000) {
+//         salary += bonus;
+//         console.log('salary R:' .concat(salary))
+//     } else{
+//         console.log('your salary is less than R5000');
+//     }
+// } else{
+//     console.log('you are not qualified')
+// }
+
+// Collection
+let items = ['Banana', 'Mango', 'Orange', 'Laptop', 'Mouse',
+'Keyboard'];
+// Linking
+// let ul = document.getElementById('items');
+// Load items to an ul
+// items.forEach( (item) => {
+//     ul.innerHTML += `
+//     <li class='bgDarkCyan'>${item}</li>
+//     `;
+//     ul
+// });
+
+
+const friends = ['ikhlaas','charles','saleem','muda','raees','yaseen','jared','ridhaa','monique','shane'];
+
+console.log(friends);
+console.log(friends[friends.length/2])
+console.log(friends[friends.length/-1])
+
+
+
+
+
